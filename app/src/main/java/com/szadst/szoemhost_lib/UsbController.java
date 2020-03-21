e19481292b194942ff79b8df7aab5e04e6b57802
Majudhu <majudhu.sup@gmail.com>
2020-03-21 15:39:15 +0500

    log usb send and receive

--- app/src/main/java/com/szadst/szoemhost_lib/UsbController.java
+++ app/src/main/java/com/szadst/szoemhost_lib/UsbController.java
@@ -43,60 +43,59 @@ import android.widget.Toast;
 
 /**
  * (c) Neuxs-Computing GmbH Switzerland
- * @author Manuel Di Cerbo, 02.02.2012
  *
+ * @author Manuel Di Cerbo, 02.02.2012
  */
 
 
 public class UsbController {
 
-	private final Context mApplicationContext;
-	private final UsbManager mUsbManager;
-	private final int VID;
-	private final int PID;
+    private final Context mApplicationContext;
+    private final UsbManager mUsbManager;
+    private final int VID;
+    private final int PID;
     private int m_nEPInSize, m_nEPOutSize;
 
     private byte[] m_abyTransferBuf;
     private boolean m_bInit = false;
-//    private UsbDevice   m_usbDevice;
+    //    private UsbDevice   m_usbDevice;
     private UsbDeviceConnection m_usbConn = null;
     private UsbInterface m_usbIf = null;
     private UsbEndpoint m_epIN = null;
     private UsbEndpoint m_epOUT = null;
     private final IUsbConnState mConnectionHandler;
 
-	protected static final String ACTION_USB_PERMISSION = "ch.serverbox.android.USB";
-    
-	/**
-	 * Activity is needed for onResult
-	 * 
-	 * @param parentActivity
-	 */
-	public UsbController(Activity parentActivity, IUsbConnState connectionHandler, int vid, int pid){
+    protected static final String ACTION_USB_PERMISSION = "ch.serverbox.android.USB";
+
+    /**
+     * Activity is needed for onResult
+     *
+     * @param parentActivity
+     */
+    public UsbController(Activity parentActivity, IUsbConnState connectionHandler, int vid, int pid) {
         mConnectionHandler = connectionHandler;
-		mApplicationContext = parentActivity.getApplicationContext();
-		mUsbManager = (UsbManager) mApplicationContext.getSystemService(Context.USB_SERVICE);
-		VID = vid;
-		PID = pid;
+        mApplicationContext = parentActivity.getApplicationContext();
+        mUsbManager = (UsbManager) mApplicationContext.getSystemService(Context.USB_SERVICE);
+        VID = vid;
+        PID = pid;
         m_abyTransferBuf = new byte[512];
 //		init();
-	}
+    }
 
-	public void init(){
-		enumerate(new IPermissionListener() {
-			@Override
-			public void onPermissionDenied(UsbDevice d) {
-				UsbManager usbman = (UsbManager) mApplicationContext.getSystemService(Context.USB_SERVICE);
-				PendingIntent pi = PendingIntent.getBroadcast(mApplicationContext, 0, new Intent(ACTION_USB_PERMISSION), 0);
-				mApplicationContext.registerReceiver(mPermissionReceiver, new IntentFilter(ACTION_USB_PERMISSION));
-				usbman.requestPermission(d, pi);
-			}
-		});
-	}
+    public void init() {
+        enumerate(new IPermissionListener() {
+            @Override
+            public void onPermissionDenied(UsbDevice d) {
+                UsbManager usbman = (UsbManager) mApplicationContext.getSystemService(Context.USB_SERVICE);
+                PendingIntent pi = PendingIntent.getBroadcast(mApplicationContext, 0, new Intent(ACTION_USB_PERMISSION), 0);
+                mApplicationContext.registerReceiver(mPermissionReceiver, new IntentFilter(ACTION_USB_PERMISSION));
+                usbman.requestPermission(d, pi);
+            }
+        });
+    }
 
-    public void uninit(){
-        if (m_usbConn != null)
-        {
+    public void uninit() {
+        if (m_usbConn != null) {
             m_usbConn.releaseInterface(m_usbIf);
             m_usbConn.close();
             m_usbConn = null;
@@ -106,94 +105,92 @@ public class UsbController {
         //stop();
     }
 
-	public void stop()
-    {
-		try{
-			mApplicationContext.unregisterReceiver(mPermissionReceiver);
-		}catch(IllegalArgumentException e){};//bravo
-	}
+    public void stop() {
+        try {
+            mApplicationContext.unregisterReceiver(mPermissionReceiver);
+        } catch (IllegalArgumentException e) {
+        }
+        ;//bravo
+    }
 
-    public boolean IsInit(){
+    public boolean IsInit() {
         return m_bInit;
     }
 
-	private void enumerate(IPermissionListener listener) {
-		boolean bFound = false;
-		l("enumerating");
-		HashMap<String, UsbDevice> devlist = mUsbManager.getDeviceList();
-		Iterator<UsbDevice> deviter = devlist.values().iterator();
+    private void enumerate(IPermissionListener listener) {
+        boolean bFound = false;
+        l("enumerating");
+        HashMap<String, UsbDevice> devlist = mUsbManager.getDeviceList();
+        Iterator<UsbDevice> deviter = devlist.values().iterator();
 
-		while (deviter.hasNext()) {
-			UsbDevice d = deviter.next();
-			l("Found device: " + String.format("%04X:%04X", d.getVendorId(), d.getProductId()));
+        while (deviter.hasNext()) {
+            UsbDevice d = deviter.next();
+            l("Found device: " + String.format("%04X:%04X", d.getVendorId(), d.getProductId()));
 
             Toast.makeText(mApplicationContext, "Found device: " + String.format("%04X:%04X", d.getVendorId(), d.getProductId()), Toast.LENGTH_SHORT).show();
 //			DebugManage.WriteLog2("Found device: " + String.format("%04X:%04X", d.getVendorId(), d.getProductId()));
 
             if (d.getVendorId() == VID && d.getProductId() == PID) {
-            	bFound = true;
-				l("Device under: " + d.getDeviceName());
-				if (!mUsbManager.hasPermission(d))
-                {
-                    Toast.makeText(mApplicationContext, "enumerate, hasPermission return false" , Toast.LENGTH_SHORT).show();
+                bFound = true;
+                l("Device under: " + d.getDeviceName());
+                if (!mUsbManager.hasPermission(d)) {
+                    Toast.makeText(mApplicationContext, "enumerate, hasPermission return false", Toast.LENGTH_SHORT).show();
                     listener.onPermissionDenied(d);
-                }
-				else{
-                    Toast.makeText(mApplicationContext, "enumerate, GetConnInerface start" , Toast.LENGTH_SHORT).show();
-					//startHandler(d);
+                } else {
+                    Toast.makeText(mApplicationContext, "enumerate, GetConnInerface start", Toast.LENGTH_SHORT).show();
+                    //startHandler(d);
                     GetConnInerface(d);
                     //TestComm(d);
-					return;
-				}
-				break;
-			}
-		}
-		if (bFound == false)
-		{
-			Toast.makeText(mApplicationContext, "no more devices found" , Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                break;
+            }
+        }
+        if (bFound == false) {
+            Toast.makeText(mApplicationContext, "no more devices found", Toast.LENGTH_SHORT).show();
 //			DebugManage.WriteLog2("no more devices found");
-			mConnectionHandler.onDeviceNotFound();
-		}
-	}
+            mConnectionHandler.onDeviceNotFound();
+        }
+    }
 
-	private class PermissionReceiver extends BroadcastReceiver {
-		private final IPermissionListener mPermissionListener;
+    private class PermissionReceiver extends BroadcastReceiver {
+        private final IPermissionListener mPermissionListener;
 
-		public PermissionReceiver(IPermissionListener permissionListener) {
-			mPermissionListener = permissionListener;
-		}
+        public PermissionReceiver(IPermissionListener permissionListener) {
+            mPermissionListener = permissionListener;
+        }
 
-		@Override
-		public void onReceive(Context context, Intent intent) {
-			mApplicationContext.unregisterReceiver(this);
-			if (intent.getAction().equals(ACTION_USB_PERMISSION)) {
-				if (!intent.getBooleanExtra(
-						UsbManager.EXTRA_PERMISSION_GRANTED, false)) {
-					mPermissionListener.onPermissionDenied((UsbDevice) intent
-							.getParcelableExtra(UsbManager.EXTRA_DEVICE));
-					
-					mConnectionHandler.onUsbPermissionDenied();
-				} else {
-					l("Permission granted");
-					UsbDevice dev = (UsbDevice) intent
-							.getParcelableExtra(UsbManager.EXTRA_DEVICE);
-					if (dev != null) {
-						if (dev.getVendorId() == VID
-								&& dev.getProductId() == PID) {
-							//startHandler(dev);// has new thread
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            mApplicationContext.unregisterReceiver(this);
+            if (intent.getAction().equals(ACTION_USB_PERMISSION)) {
+                if (!intent.getBooleanExtra(
+                        UsbManager.EXTRA_PERMISSION_GRANTED, false)) {
+                    mPermissionListener.onPermissionDenied((UsbDevice) intent
+                            .getParcelableExtra(UsbManager.EXTRA_DEVICE));
+
+                    mConnectionHandler.onUsbPermissionDenied();
+                } else {
+                    l("Permission granted");
+                    UsbDevice dev = (UsbDevice) intent
+                            .getParcelableExtra(UsbManager.EXTRA_DEVICE);
+                    if (dev != null) {
+                        if (dev.getVendorId() == VID
+                                && dev.getProductId() == PID) {
+                            //startHandler(dev);// has new thread
                             GetConnInerface(dev);
                             //TestComm(dev);
-						}
-					} else {
+                        }
+                    } else {
 //						DebugManage.WriteLog2("device not present!");
                         mConnectionHandler.onDeviceNotFound();
-					}
-				}
-			}
-		}
-	}
+                    }
+                }
+            }
+        }
+    }
 
-    private void GetConnInerface(UsbDevice dev){
+    private void GetConnInerface(UsbDevice dev) {
         int n;
 
         //Toast.makeText(mApplicationContext, "GetConnInerface start", Toast.LENGTH_SHORT).show();
@@ -240,13 +237,12 @@ public class UsbController {
         mConnectionHandler.onUsbConnected();
     }
 
-    public boolean OperationInternal(byte[] pData, int nDataLen, int nTimeOut, boolean bRead)
-    {
+    public boolean OperationInternal(byte[] pData, int nDataLen, int nTimeOut, boolean bRead) {
         byte[] w_abyTmp = new byte[31];
         byte[] w_abyCSW = new byte[13];
         boolean w_bRet;
 
-        Arrays.fill(w_abyTmp, (byte)0);
+        Arrays.fill(w_abyTmp, (byte) 0);
         w_abyTmp[0] = 0x55;
         w_abyTmp[1] = 0x53;
         w_abyTmp[2] = 0x42;
@@ -254,25 +250,25 @@ public class UsbController {
         w_abyTmp[4] = 0x28;
         w_abyTmp[5] = 0x2b;
         w_abyTmp[6] = 0x18;
-        w_abyTmp[7] = (byte)0x89;
-        w_abyTmp[8] = (byte)(nDataLen & 0xFF);
-        w_abyTmp[9] = (byte)((nDataLen >> 8)& 0xFF);
-        w_abyTmp[10] = (byte)((nDataLen >> 16)& 0xFF);
-        w_abyTmp[11] = (byte)((nDataLen >> 24)& 0xFF);
+        w_abyTmp[7] = (byte) 0x89;
+        w_abyTmp[8] = (byte) (nDataLen & 0xFF);
+        w_abyTmp[9] = (byte) ((nDataLen >> 8) & 0xFF);
+        w_abyTmp[10] = (byte) ((nDataLen >> 16) & 0xFF);
+        w_abyTmp[11] = (byte) ((nDataLen >> 24) & 0xFF);
 
-        if(bRead)
-            w_abyTmp[12] = (byte)0x80;
+        if (bRead)
+            w_abyTmp[12] = (byte) 0x80;
         else
             w_abyTmp[12] = 0x00;    //cCBWFlags
 
         w_abyTmp[13] = 0x00;    //cCBWlun
         w_abyTmp[14] = 0x0a;    //cCBWCBLength
 
-        w_abyTmp[15] = (byte)0xef;
+        w_abyTmp[15] = (byte) 0xef;
         if (bRead)
-            w_abyTmp[16] = (byte)0xff;
+            w_abyTmp[16] = (byte) 0xff;
         else
-            w_abyTmp[16] = (byte)0xfe;
+            w_abyTmp[16] = (byte) 0xfe;
 
         // send 31bytes
         w_bRet = UsbBulkSend(w_abyTmp, 31, nTimeOut);
@@ -295,8 +291,7 @@ public class UsbController {
         return w_bRet;
     }
 
-    public boolean UsbSCSIWrite(byte[] pCDB, int nCDBLen, byte[] pData, int nDataLen, int nTimeOut)
-    {
+    public boolean UsbSCSIWrite(byte[] pCDB, int nCDBLen, byte[] pData, int nDataLen, int nTimeOut) {
         byte[] w_abyTmp = new byte[31];
         byte[] w_abyCSW = new byte[13];
         boolean w_bRet;
@@ -309,7 +304,7 @@ public class UsbController {
         w_abyTmp[4] = 0x28;
         w_abyTmp[5] = 0x2b;
         w_abyTmp[6] = 0x18;
-        w_abyTmp[7] = (byte)0x89;
+        w_abyTmp[7] = (byte) 0x89;
         w_abyTmp[8] = 0x00;
         w_abyTmp[9] = 0x00;
         w_abyTmp[10] = 0x00;
@@ -330,16 +325,15 @@ public class UsbController {
 
         if (!w_bRet)
             return false;
-        
+
         // receive csw
         w_bRet = UsbBulkReceive(w_abyCSW, 13, nTimeOut);
 
-    	return w_bRet;
+        return w_bRet;
     }
 
-    public boolean UsbSCSIRead(byte[] pCDB, int nCDBLen, byte[] pData, int nDataLen, int nTimeOut)
-    {
-        long    w_nTime;
+    public boolean UsbSCSIRead(byte[] pCDB, int nCDBLen, byte[] pData, int nDataLen, int nTimeOut) {
+        long w_nTime;
         byte[] w_abyTmp = new byte[31];
         byte[] w_abyCSW = new byte[13];
         boolean w_bRet;
@@ -352,12 +346,12 @@ public class UsbController {
         w_abyTmp[4] = 0x28;
         w_abyTmp[5] = 0x2b;
         w_abyTmp[6] = 0x18;
-        w_abyTmp[7] = (byte)0x89;
+        w_abyTmp[7] = (byte) 0x89;
         w_abyTmp[8] = 0x00;
         w_abyTmp[9] = 0x00;
         w_abyTmp[10] = 0x00;
         w_abyTmp[11] = 0x00;
-        w_abyTmp[12] = (byte)0x80;    //cCBWFlags
+        w_abyTmp[12] = (byte) 0x80;    //cCBWFlags
         w_abyTmp[13] = 0x00;    //cCBWlun
         w_abyTmp[14] = 0x0a;    //cCBWCBLength
 
@@ -387,17 +381,15 @@ public class UsbController {
         return w_bRet;
     }
 
-    private boolean UsbBulkSend(byte[] pBuf, int nLen, int nTimeOut)
-    {
+    private boolean UsbBulkSend(byte[] pBuf, int nLen, int nTimeOut) {
         int i, n, r, w_nRet;
         //byte[] w_abyTmp = new byte[m_nEPOutSize];
 
         n = nLen / m_nEPOutSize;
         r = nLen % m_nEPOutSize;
 
-        for(i=0; i<n; i++)
-        {
-            System.arraycopy(pBuf, i*m_nEPOutSize, m_abyTransferBuf, 0, m_nEPOutSize);
+        for (i = 0; i < n; i++) {
+            System.arraycopy(pBuf, i * m_nEPOutSize, m_abyTransferBuf, 0, m_nEPOutSize);
 
             w_nRet = m_usbConn.bulkTransfer(m_epOUT, m_abyTransferBuf, m_nEPOutSize, nTimeOut);
 
@@ -405,11 +397,29 @@ public class UsbController {
                 return false;
         }
 
-        if (r > 0)
-        {
-            System.arraycopy(pBuf, i*m_nEPOutSize, m_abyTransferBuf, 0, r);
+        if (r > 0) {
+            System.arraycopy(pBuf, i * m_nEPOutSize, m_abyTransferBuf, 0, r);
 
             w_nRet = m_usbConn.bulkTransfer(m_epOUT, m_abyTransferBuf, r, nTimeOut);
+            Log.d("usb-data", String.format("cmd:%2d:%2d : %02X%02X %02X%02X %02X%02X %02X%02X %02X%02X %02X%02X %02X%02X %02X%02X %02X%02X %02X%02X %02X%02X %02X%02X %02X%02X %02X%02X %02X%02X %02X%02X ",
+                    r, w_nRet,
+                    m_abyTransferBuf[1],   m_abyTransferBuf[0],
+                    m_abyTransferBuf[3],   m_abyTransferBuf[2],
+                    m_abyTransferBuf[5],   m_abyTransferBuf[4],
+                    m_abyTransferBuf[7],   m_abyTransferBuf[6],
+                    m_abyTransferBuf[9],   m_abyTransferBuf[8],
+                    m_abyTransferBuf[11],  m_abyTransferBuf[10],
+                    m_abyTransferBuf[13],  m_abyTransferBuf[12],
+                    m_abyTransferBuf[15],  m_abyTransferBuf[14],
+                    m_abyTransferBuf[17],  m_abyTransferBuf[16],
+                    m_abyTransferBuf[19],  m_abyTransferBuf[18],
+                    m_abyTransferBuf[21],  m_abyTransferBuf[20],
+                    m_abyTransferBuf[23],  m_abyTransferBuf[22],
+                    m_abyTransferBuf[25],  m_abyTransferBuf[24],
+                    m_abyTransferBuf[27],  m_abyTransferBuf[26],
+                    m_abyTransferBuf[29],  m_abyTransferBuf[28],
+                    m_abyTransferBuf[31],  m_abyTransferBuf[30]
+            ));
 
             if (w_nRet != r)
                 return false;
@@ -418,8 +428,7 @@ public class UsbController {
         return true;
     }
 
-    private boolean UsbBulkReceive(byte[] pBuf, int nLen, int nTimeOut)
-    {
+    private boolean UsbBulkReceive(byte[] pBuf, int nLen, int nTimeOut) {
         int i, n, r, w_nRet;
         //byte[] w_abyTmp = new byte[m_nEPInSize];
 
@@ -433,49 +442,65 @@ public class UsbController {
 
         //Toast.makeText(mApplicationContext, "UsbBulkReceive, Buf Len = " + pBuf.length, Toast.LENGTH_SHORT).show();
 
-        for(i=0; i<n; i++)
-        {
+        for (i = 0; i < n; i++) {
             w_nRet = m_usbConn.bulkTransfer(m_epIN, m_abyTransferBuf, m_nEPInSize, nTimeOut);
 
             if (w_nRet != m_nEPInSize) {
                 return false;
             }
 
-            System.arraycopy(m_abyTransferBuf, 0, pBuf, i*m_nEPInSize, m_nEPInSize);
+            System.arraycopy(m_abyTransferBuf, 0, pBuf, i * m_nEPInSize, m_nEPInSize);
         }
 
-        if (r > 0)
-        {
+        if (r > 0) {
             w_nRet = m_usbConn.bulkTransfer(m_epIN, m_abyTransferBuf, r, nTimeOut);
-
+            Log.d("usb-data", String.format("res:%2d:%2d : %02X%02X %02X%02X %02X%02X %02X%02X %02X%02X %02X%02X %02X%02X %02X%02X %02X%02X %02X%02X %02X%02X %02X%02X %02X%02X %02X%02X %02X%02X %02X%02X ",
+                    r, w_nRet,
+                    m_abyTransferBuf[1],   m_abyTransferBuf[0],
+                    m_abyTransferBuf[3],   m_abyTransferBuf[2],
+                    m_abyTransferBuf[5],   m_abyTransferBuf[4],
+                    m_abyTransferBuf[7],   m_abyTransferBuf[6],
+                    m_abyTransferBuf[9],   m_abyTransferBuf[8],
+                    m_abyTransferBuf[11],  m_abyTransferBuf[10],
+                    m_abyTransferBuf[13],  m_abyTransferBuf[12],
+                    m_abyTransferBuf[15],  m_abyTransferBuf[14],
+                    m_abyTransferBuf[17],  m_abyTransferBuf[16],
+                    m_abyTransferBuf[19],  m_abyTransferBuf[18],
+                    m_abyTransferBuf[21],  m_abyTransferBuf[20],
+                    m_abyTransferBuf[23],  m_abyTransferBuf[22],
+                    m_abyTransferBuf[25],  m_abyTransferBuf[24],
+                    m_abyTransferBuf[27],  m_abyTransferBuf[26],
+                    m_abyTransferBuf[29],  m_abyTransferBuf[28],
+                    m_abyTransferBuf[31],  m_abyTransferBuf[30]
+            ));
             if (w_nRet != r) {
                 return false;
             }
 
-            System.arraycopy(m_abyTransferBuf, 0, pBuf, i*m_nEPInSize, r);
+            System.arraycopy(m_abyTransferBuf, 0, pBuf, i * m_nEPInSize, r);
         }
 
         return true;
     }
 
-	// END MAIN LOOP
-	private BroadcastReceiver mPermissionReceiver = new PermissionReceiver(
-			new IPermissionListener() {
-				@Override
-				public void onPermissionDenied(UsbDevice d) {
-					l("Permission denied on " + d.getDeviceId());
-				}
-			});
+    // END MAIN LOOP
+    private BroadcastReceiver mPermissionReceiver = new PermissionReceiver(
+            new IPermissionListener() {
+                @Override
+                public void onPermissionDenied(UsbDevice d) {
+                    l("Permission denied on " + d.getDeviceId());
+                }
+            });
 
-	private static interface IPermissionListener {
-		void onPermissionDenied(UsbDevice d);
-	}
+    private static interface IPermissionListener {
+        void onPermissionDenied(UsbDevice d);
+    }
 
-	public final static String TAG = "USBController";
+    public final static String TAG = "USBController";
 
-	private void l(Object msg) {
-		Log.d(TAG, ">==< " + msg.toString() + " >==<");
-	}
+    private void l(Object msg) {
+        Log.d(TAG, ">==< " + msg.toString() + " >==<");
+    }
 
 //	private void e(Object msg) {
 //		Log.e(TAG, ">==< " + msg.toString() + " >==<");
